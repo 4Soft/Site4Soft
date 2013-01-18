726ce0571e1bfb5c8fd8ac1cd3bd1e3415274df6
Igor Marques da Silva <igormarquessilva@gmail.com>
2013-01-18 13:42:00 -0300

    Primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Site4Soft::Application.config.secret_token = '249bae3eeadc5be496fbc7ccf4ce9d9ab676803caf1fb59750ea76200e97003c9ed77bb62c13005f4853a4ba9f518897bfdda982a7fcf0a3763a7a5e65a038d7'
