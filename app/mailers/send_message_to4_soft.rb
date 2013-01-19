240c00816f4485f2f00f0774af37203308d56e96
painkiller778 <painkiller778@gmail.com>
2013-01-19 12:22:14 -0300

    Enviando email para 4softjr@gmail.com agora

--- app/mailers/send_message_to4_soft.rb
+++ app/mailers/send_message_to4_soft.rb
@@ -3,6 +3,6 @@ class SendMessageTo4Soft < ActionMailer::Base
 
   def send_message_to_gmail_group(message)
     @message = message
-    mail to: "4softjr@googlegroups.com", subject: "Nova mensagem enviada pelo site"
+    mail to: "4softjr@gmail.com", subject: "Nova mensagem enviada pelo site"
   end
 end
